ff56120ed50f1d4cc112f806a58ae28f80d29b72
Peter Mbanugo <chuksgerrard@yahoo.com>
2020-11-10 23:17:18 +0100

    upgrade to hamoni version 1.1.1

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,3 @@
-let hamoni = new Hamoni("ACCOUNT_ID", "APP_ID");
-
-hamoni
-  .connect()
-  .then(response => {
-    hamoni
-      .get("election2018")
-      .then(statePrimitive => {
-        console.log("state retrieved");
-
-        renderChart(statePrimitive.get());
-
-        statePrimitive.onUpdated(state => renderChart(state));
-      })
-      .catch(console.log);
-  })
-  .catch(error => console.log(error));
-
 function renderChart(state) {
   var ctx = document.getElementById("myChart").getContext("2d");
   var chart = new Chart(ctx, {
@@ -23,15 +5,15 @@ function renderChart(state) {
     type: "bar",
     // The data for our dataset
     data: {
-      labels: state.map(s => s.candidate),
+      labels: state.map((s) => s.candidate),
       datasets: [
         {
           label: "Elections 2018",
           backgroundColor: "rgb(255, 99, 132)",
           borderColor: "rgb(255, 99, 132)",
-          data: state.map(s => s.vote)
-        }
-      ]
+          data: state.map((s) => s.vote),
+        },
+      ],
     },
     // Configuration options go here
     options: {
@@ -39,32 +21,67 @@ function renderChart(state) {
         xAxes: [
           {
             time: {
-              unit: "Vote"
+              unit: "Vote",
             },
             gridLines: {
-              display: false
+              display: false,
             },
             ticks: {
-              maxTicksLimit: 6
-            }
-          }
+              maxTicksLimit: 6,
+            },
+          },
         ],
         yAxes: [
           {
             ticks: {
               min: 0,
               max: 30,
-              maxTicksLimit: 10
+              maxTicksLimit: 10,
             },
             gridLines: {
-              display: true
-            }
-          }
-        ]
+              display: true,
+            },
+          },
+        ],
       },
       legend: {
-        display: true
-      }
-    }
+        display: true,
+      },
+    },
   });
 }
+
+async function execute() {
+  const accountId = "YOUR_ACCOUNT_ID";
+  const appId = "YOUR_APP_ID";
+
+  const response = await fetch("https://api.sync.hamoni.tech/v1/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ accountId, appId }),
+  });
+
+  const token = await response.json();
+
+  let hamoni = new Hamoni(token);
+
+  hamoni
+    .connect()
+    .then((response) => {
+      hamoni
+        .get("election2018")
+        .then((statePrimitive) => {
+          console.log("state retrieved");
+
+          renderChart(statePrimitive.get());
+
+          statePrimitive.onUpdated((state) => renderChart(state));
+        })
+        .catch(console.log);
+    })
+    .catch((error) => console.log(error));
+}
+
+execute();
--- server.js
+++ server.js
@@ -10,12 +10,12 @@ let voteData = [
   { candidate: "Peter Mbanugo", vote: 0 },
   { candidate: "Angela Daniels", vote: 0 },
   { candidate: "Rose Philly", vote: 0 },
-  { candidate: "James Crump", vote: 0 }
+  { candidate: "James Crump", vote: 0 },
 ];
 
 let statePrimitive;
 
-app.post("/vote", function(req, res) {
+app.post("/vote", function (req, res) {
   var voterId = req.body.voterId;
   var candidate = req.body.candidate;
   console.log(candidate);
@@ -23,7 +23,7 @@ app.post("/vote", function(req, res) {
   //TODO: save vote to a database
 
   //Update vote chart state
-  voteData.forEach(data => {
+  voteData.forEach((data) => {
     if (data.candidate === candidate) data.vote = data.vote + 1;
   });
 
@@ -36,37 +36,54 @@ app.post("/vote", function(req, res) {
 app.use(express.static(__dirname + "/public"));
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`app listening on port ${port}!`);
 });
-
-let hamoni = new Hamoni("ACCOUNT_ID", "APP_ID");
-
-hamoni
-  .connect()
-  .then(() => {
-    console.log("connected to Hamoni sync");
-    createState();
-  })
-  .catch(console.log);
 
 function createState() {
   hamoni
     .createObject("election", voteData)
-    .then(statePrimitive => {
+    .then((statePrimitive) => {
       console.log("election state created");
       state = statePrimitive;
     })
-    .catch(error => console.log(error) || getState());
+    .catch((error) => console.log(error) || getState());
 }
 
 function getState() {
   hamoni
     .get("election")
-    .then(statePrimitive => {
+    .then((statePrimitive) => {
       console.log("election state retrieved");
       state = statePrimitive;
       voteData = statePrimitive.get();
     })
     .catch(console.log);
 }
+
+async function execute() {
+  const accountId = "YOUR_ACCOUNT_ID";
+  const appId = "YOUR_APP_ID";
+
+  const response = await fetch("https://api.sync.hamoni.tech/v1/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({ accountId, appId }),
+  });
+
+  const token = await response.json();
+
+  let hamoni = new Hamoni(token);
+
+  hamoni
+    .connect()
+    .then(() => {
+      console.log("connected to Hamoni sync");
+      createState();
+    })
+    .catch(console.log);
+}
+
+execute();
